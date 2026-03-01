6b8268ba035f226edc889fce2363109d77bb8754
Rahmat-Ali-15 <developer.rahmat15@gmail.com>
2026-03-01 17:22:53 +0530

    Code of Navbar

---
+++ SkillOps/src/Components/Navbar.jsx
@@ -0,0 +1,31 @@
+import { NavLink } from "react-router-dom";
+
+export const Navbar = () => {
+
+    const route = [
+        {path: "/", title: "Home"},
+        {path: "/ourcourse", title: "Our Course"},
+        {path: "/placement", title: "Placement"},
+        {path: "/contact", title: "Contact"},
+        {path: "/about", title: "About"}
+    ]
+
+
+  return (
+    <>
+      <div className="relative h-30 flex items-center justify-center">
+        <div className="absolute top-7 flex w-[80%] h-15 items-center justify-between pl-7 pr-2 rounded-full bg-black text-white">
+          <div>Logo</div>
+          <div className="flex gap-15">
+            {route.map((el,id) => (
+                <NavLink className= "text-[18px]" key={id} to = {el.path} end>{el.title}</NavLink>
+            ))}
+          </div>
+          <div className="px-10 py-3 bg-[#ec0d14] rounded-4xl font-bold hover:bg-[#b30a10] transition">
+            <button className="text-[18px]">Login / Sign Up</button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
