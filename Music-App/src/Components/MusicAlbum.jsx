92799f11a4e74294a19ef1bfbc69f0315b357eee
Rahmat-Ali-15 <developer.rahmat15@gmail.com>
2026-03-10 12:35:32 +0530

    Code of Music Album

---
+++ Music-App/src/Components/MusicAlbum.jsx
@@ -0,0 +1,31 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux"
+
+
+import { getMusicRecords } from './../Redux/actionTypes';
+
+export const MusicAlbum = () => {
+    const dispatch = useDispatch();
+    const musicData = useSelector(store => store.musicRecords)
+
+
+    useEffect(() => {
+        dispatch(getMusicRecords)
+    },[])
+
+
+    return (
+        <>
+            {
+                musicData.length > 0 && musicData.map((album) => {
+                    return <div key={album.id}>
+                        <h3>{album.id}</h3>
+                        <h3>{album.name}</h3>
+                        <h3>{album.genre}</h3>
+                        <img src={album.img} alt={album.name} />
+                    </div>
+                })
+            }
+        </>
+    )
+}
