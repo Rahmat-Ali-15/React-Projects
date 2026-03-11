286f06afcfb00d663f7fd6260d6886557113d750
Rahmat-Ali-15 <developer.rahmat15@gmail.com>
2026-03-11 20:31:43 +0530

    Modified code

--- Music-App/src/Components/FilterMusicRecords.jsx
+++ Music-App/src/Components/FilterMusicRecords.jsx
@@ -1,12 +1,40 @@
 import { useState } from "react";
 import { useSearchParams } from "react-router-dom"
 
+import data from "../../db.json"
 export const FilterMusicRecords = () => {
 
     const [searchParams, setSearchParams] = useSearchParams();
-    const [filterVal, setFilterVal] = useState([])
-    console.log("🚀 ~ filterVal:", filterVal);
+    console.log("🚀 ~ setSearchParams:", setSearchParams);
+    console.log("🚀 ~ searchParams:", searchParams);
 
+    const [filterVal, setFilterVal] = useState([])
+
+    let genre = []
+    data.albums.map((el) => {
+        genre.push(el.genre)
+    })
+    console.log("🚀 ~ genre:", genre);
+
+    let obj = {}
+
+    genre.reduce((curr, acc) => {
+        curr[acc] = (curr[acc] || 0) + 1
+        return curr
+    },obj)
+    console.log("🚀 ~ obj:", obj);
+    
+    // for(let i = 0; i < arr.length; i++){
+    //     let isGenre = arr[i]
+    //     if(obj[isGenre]){
+    //         obj[isGenre]++
+    //     }
+    //     else{
+    //         obj[isGenre] = 1
+    //     }
+    // }
+    // console.log("🚀 ~ obj:", obj);
+    
     const handleFilter = (e) => {
         const option = e.target.name;
         console.log("🚀 ~ option:", option);
@@ -28,18 +56,15 @@ export const FilterMusicRecords = () => {
         <>
             <h1>Filter</h1>
             <div style={{display: "flex", alignItems: "start", flexDirection: "column"}}>
-                <div>
-                    <input type="checkbox" name="k-pop" onChange={handleFilter} />
-                    <label>K-Pop</label>
-                </div>
-                <div>
-                    <input type="checkbox" name="Holiday" onChange={handleFilter} />
-                    <label>Holiday</label>
-                </div>
-                <div>
-                    <input type="checkbox" name="Heavy Metal" onChange={handleFilter} />
-                    <label>Heavy Metal</label>
-                </div>
+            {
+                Object.keys(obj).map((el) => {
+                    return <div key={el}>
+                        <input type="checkbox" name={el} onChange={handleFilter} />
+                        <label>{el}</label>
+                    </div>;
+                })
+
+            }
             </div>
         </>
     )
