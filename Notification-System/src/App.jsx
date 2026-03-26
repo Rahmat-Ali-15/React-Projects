b0b01209116138c3578ae7836bcbc475e3981313
Rahmat-Ali-15 <developer.rahmat15@gmail.com>
2026-03-26 21:08:14 +0530

    Code

---
+++ Notification-System/src/App.jsx
@@ -0,0 +1,13 @@
+// import { Form } from "./Components/Form"
+
+import { Notification } from "./Components/Notification"
+import { Login } from "./Pages/Login"
+
+export const App = () => {
+  return(
+    <>
+      <Login />
+      <Notification />
+    </>
+  )
+}
